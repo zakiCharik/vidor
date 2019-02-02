2064acdf6cef1f3ea0d11de27e462e8b121d52cb
root <root@vps.server.local>
2019-02-02 13:22:32 +0000

    certificate https and key verification google map

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var mySqlClient = mysql.createConnection({
   user      : process.env.DB_USER,
   password  : process.env.DB_PASSWORD,
   database  : process.env.DB_DATABASE,
-  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+  socketPath: '/var/run/mysqld/mysqld.sock'
   // socketPath: process.env.DB_SOCKET_PATH
 });
 
